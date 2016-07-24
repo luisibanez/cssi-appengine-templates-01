9201311add888b5bc1686e96571cb41760ef747d
Luis Ibanez <luis.ibanez@gmail.com>
2016-07-24 12:00:37 -0400

    Add minimal example of variables use in templates.

---
+++ src/example04/main.py
@@ -0,0 +1,19 @@
+import jinja2
+import os
+import webapp2
+
+
+jinja_environment = jinja2.Environment(loader=
+    jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        template_vars = {"name": "CSSI NYC"}
+        template = jinja_environment.get_template('templates/main.html')
+        self.response.write(template.render(template_vars))
+
+
+app = webapp2.WSGIApplication([
+  ('/', MainHandler),
+], debug=True)
