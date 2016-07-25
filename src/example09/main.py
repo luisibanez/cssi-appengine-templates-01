0894d4e306065aaba7465c68d545890bee49386d
Luis Ibanez <luis.ibanez@gmail.com>
2016-07-24 20:16:48 -0400

    Add example on using loops in a template.

---
+++ src/example09/main.py
@@ -0,0 +1,20 @@
+import jinja2
+import os
+import webapp2
+
+
+jinja_environment = jinja2.Environment(loader=
+    jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        template_vars = {
+            'names': ['John', 'Paul', 'George', 'Ringo']}
+        template = jinja_environment.get_template('templates/main.html')
+        self.response.write(template.render(template_vars))
+
+
+app = webapp2.WSGIApplication([
+  ('/', MainHandler),
+], debug=True)
